0ff08cea859bb1da31194167250a875466cb033c
plexin123 <archimago05@hotmail.com>
2025-07-23 18:57:51 -0700

    idea

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -11,7 +11,7 @@ fn get_char(data: String) -> char {
 // Should take ownership
 fn string_uppercase(mut data: &String) {
     data = data.to_uppercase();
-
+    println!("idea");
     println!("{data}");
 }
 
